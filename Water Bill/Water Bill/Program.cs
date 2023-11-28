9515058eff1f416afa1179c414417329871c6800
Halkano-Arero <150130174+Halkano-Arero@users.noreply.github.com>
2023-11-28 09:18:21 +0300

    Add files via upload

---
+++ Water Bill/Water Bill/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Water_Bill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do {
+                try {
+
+                    Console.WriteLine("                                                              Welcome To Water Bill Calculator!");
+                    Console.WriteLine("                                                            .......................................");
+                    string name;
+                    double type;
+                    double amount;
+                    double rate;
+                    double cost;
+                   
+                    
+                    Console.WriteLine("Enter The Consumer Name:");
+                    name = Console.ReadLine();
+
+                    Console.WriteLine("Enter you Consumption Amount:\t");
+                    amount = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"The Amount of water consumed is:{amount}");
+                    try { 
+                    Console.WriteLine("Choose Your Type Of Usage");
+                    Console.WriteLine("\t1=Commercial");
+                    Console.WriteLine("\t2=Industrial");
+                    Console.WriteLine("\t3=domestic");
+                    Console.WriteLine("Enter Your Code Type:");
+                    type = double.Parse(Console.ReadLine());
+
+                    
+                        if (type == 1)
+                        {
+                            rate = 25.00;
+                            Console.WriteLine($"Your Bill is:\n{rate}*{amount} = {rate * amount}", rate, amount);
+                        }
+                        else if (type == 2)
+                        {
+                            rate = 30.50;
+                            Console.WriteLine($"Your Bill is:\n{rate}*{amount} = {rate * amount}", rate, amount);
+                        }
+                        else if (type == 3)
+                        {
+                            rate = 35.75;
+                            Console.WriteLine($"Your Bill is:\n{rate}*{amount} = {rate * amount}", rate, amount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Input! Check And Try Again!");
+                        }
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("\tInvalid Code!");
+                    }
+
+                    Console.ReadKey();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\tInvalid input!");
+                }
+
+                Console.WriteLine("Do you want to continue?(Y=yes,N=no):");
+            }
+            while (Console.ReadLine().ToUpper() == "Y");
+            Console.WriteLine("Bye!");
+
+            Console.ReadKey(true);
+        }
+    }
+}
